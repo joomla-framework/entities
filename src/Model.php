113a769164e24241c82445b81501a7c9b2acef71
Isac Andrei <isacandrei19@gmail.com>
2018-05-27 18:27:25 +0200

    impemented ArrayAccess in Model

--- src/Model.php
+++ src/Model.php
@@ -8,18 +8,18 @@
 
 namespace Joomla\Entity;
 
+use ArrayAccess;
+use JsonSerializable;
 use Joomla\Database\DatabaseFactory;
 use Joomla\Database\DatabaseDriver;
 use Joomla\String\Inflector;
-
-// TODO Should implement ArrayAccess, JsonSerializable
 
 /**
  * Class Model
  * @package Joomla\Entity
  * @since 1.0
  */
-abstract class Model
+abstract class Model implements ArrayAccess, JsonSerializable
 {
 	use Helpers\Attributes;
 
@@ -207,6 +207,8 @@ abstract class Model
 	{
 		$this->setAttribute($key, $value);
 	}
+
+
 
 	/**
 	 * @return boolean
@@ -450,4 +452,70 @@ abstract class Model
 		$this->table = '#__' . Inflector::pluralize($className);
 	}
 
+	/**
+	 * Determine if the given attribute exists.
+	 *
+	 * @param   mixed  $offset ?
+	 * @return boolean
+	 */
+	public function offsetExists($offset)
+	{
+		return ! is_null($this->getAttribute($offset));
+	}
+
+	/**
+	 * Get the value for a given offset.
+	 *
+	 * @param   mixed  $offset ?
+	 * @return mixed
+	 */
+	public function offsetGet($offset)
+	{
+		return $this->getAttribute($offset);
+	}
+
+	/**
+	 * Set the value for a given offset.
+	 *
+	 * @param   mixed  $offset ?
+	 * @param   mixed  $value  ?
+	 * @return void
+	 */
+	public function offsetSet($offset, $value)
+	{
+		$this->setAttribute($offset, $value);
+	}
+
+	/**
+	 * Unset the value for a given offset.
+	 *
+	 * @param   mixed  $offset ?
+	 * @return void
+	 */
+	public function offsetUnset($offset)
+	{
+		unset($this->attributes[$offset], $this->relations[$offset]);
+	}
+
+	/**
+	 * Determine if an attribute or relation exists on the model.
+	 *
+	 * @param   string  $key attribute name
+	 * @return boolean
+	 */
+	public function __isset($key)
+	{
+		return $this->offsetExists($key);
+	}
+
+	/**
+	 * Unset an attribute on the model.
+	 *
+	 * @param   string  $key attribute name
+	 * @return void
+	 */
+	public function __unset($key)
+	{
+		$this->offsetUnset($key);
+	}
 }
