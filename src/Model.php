a3ab740196e489206c31b4fbd7d87bc65e2e783e
Isac Andrei <isacandrei19@gmail.com>
2018-07-09 19:44:00 +0300

    integrating Joomla CMS needed functionality into the model

--- src/Model.php
+++ src/Model.php
@@ -124,6 +124,8 @@ abstract class Model implements ArrayAccess, JsonSerializable
 
 		$this->setAttributes($attributes);
 
+		$this->syncOriginal();
+
 		$this->casts[$this->primaryKey] = $this->primaryKeyType;
 	}
 
@@ -847,5 +849,81 @@ abstract class Model implements ArrayAccess, JsonSerializable
 		{
 			$this->setAttributes($attributes);
 		}
+
+		$this->syncOriginal();
+	}
+
+	/**
+	 * Load a row in the current insance
+	 *
+	 * @param   mixed    $key    primary key, if there is no key, then this is used for a new item, therefore select last
+	 * @param   boolean  $reset  reset flag
+	 *
+	 * @return boolean
+	 */
+	public function load($key = null, $reset = true)
+	{
+		$key = ($key) ?: $this->getPrimaryKeyValue();
+
+		if ($key === null)
+		{
+			throw new \UnexpectedValueException('Null primary key not allowed.');
+		}
+
+		$query = $this->newQuery();
+
+		if ($reset)
+		{
+			$this->reset();
+		}
+
+		$this->setAttributes($query->selectRaw($key));
+
+		$this->exists = true;
+
+		$this->syncOriginal();
+
+		return true;
+	}
+
+	/**
+	 * Check function. This need to be overwritten in the Entity class.
+	 *
+	 * @return boolean
+	 */
+	public function check()
+	{
+		return true;
+	}
+
+	/**
+	 * Bind function, useful because it only sets existing attributes
+	 *
+	 * @param   array  $src     assoc array of values for binding
+	 * @param   array  $ignore  keys to be ignored
+	 *
+	 * @return boolean
+	 */
+	public function bind($src, $ignore = array())
+	{
+		if (is_string($ignore))
+		{
+			$ignore = explode(' ', $ignore);
+		}
+
+		// Bind the source value, excluding the ignored fields.
+		foreach ($this->getAttributes() as $k => $v)
+		{
+			// Only process fields not in the ignore array.
+			if (!in_array($k, $ignore))
+			{
+				if (isset($src[$k]))
+				{
+					$this->setAttribute($k, $src[$k]);
+				}
+			}
+		}
+
+		return true;
 	}
 }
