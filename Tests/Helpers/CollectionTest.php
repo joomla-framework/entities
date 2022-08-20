a727060910a38a7e930338f2767f17630da718ac
Niels Braczek <nbraczek@bsds.de>
2022-08-20 12:57:07 +0200

    Fix namespace

--- Tests/Helpers/CollectionTest.php
+++ Tests/Helpers/CollectionTest.php
@@ -9,6 +9,7 @@ namespace Joomla\Entity\Tests\Helpers;
 use Joomla\Entity\Tests\Models\User;
 use Joomla\Entity\Helpers\Collection;
 use Joomla\Entity\Tests\Models\UserProfile;
+use Joomla\Entity\Tests\SqliteCase;
 
 /**
  *
